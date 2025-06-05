b4c0359a1d7c881ae6481f31b8af69d2db42ee01
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-06-05 06:23:05 +0545

    newsitem

--- React-Project/src/App.jsx
+++ React-Project/src/App.jsx
@@ -3,11 +3,13 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./component/Navbar";
+import News from "./component/News";
 
 function App() {
   return (
     <>
       <Navbar />
+      <News />
     </>
   );
 }
---
+++ React-Project/src/component/News.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Newsitem from "./Newsitem";
+
+const News = () => {
+  return (
+    <>
+      <p>This is a news component</p>
+      <Newsitem />
+      <Newsitem />
+      <Newsitem />
+      <Newsitem />
+      <Newsitem />
+      <Newsitem />
+    </>
+  );
+};
+
+export default News;
