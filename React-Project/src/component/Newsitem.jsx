8844af09ec3bf739ff54ac9625d7120658da0d11
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-06-05 06:41:23 +0545

    props

--- React-Project/src/component/Newsitem.jsx
+++ React-Project/src/component/Newsitem.jsx
@@ -1,22 +1,15 @@
 import React from "react";
 
-const Newsitem = () => {
+const Newsitem = (props) => {
   return (
     <div>
       <div className="card" style={{ width: "18rem" }}>
-        <img
-          src="https://via.placeholder.com/150"
-          className="card-img-top"
-          alt="..."
-        />
+        <img src={props.imageUrl} className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card’s content.
-          </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
+          <h5 className="card-title">{props.title}</h5>
+          <p className="card-text">{props.description}</p>
+          <a href="#" className="btn btn-sm btn-primary">
+            Read More
           </a>
         </div>
       </div>
