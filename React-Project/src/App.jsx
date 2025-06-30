eab8e72d045f8da184b02e65fa408649798dd514
JoshiNirdesh <joshinirdesh10@gmail.com>
2025-06-30 06:27:04 +0545

    compoents

--- React-Project/src/App.jsx
+++ React-Project/src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import Navbar from "./component/Navbar";
-import News from "./component/News";
+import Content from "./component/Content";
 
 function App() {
   return (
     <>
       <Navbar />
-      <News />
+      <Content />
     </>
   );
 }
---
+++ React-Project/src/component/Content.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Content = () => {
+  return (
+    <div>
+      <h1>This is a content section </h1>
+    </div>
+  );
+};
+
+export default Content;
